91912e30aab91ad8b20084a8b3e43369bfd25d3f
Jblomp <71639284+Jblomp@users.noreply.github.com>
2021-11-04 11:03:27 +0100

    Adding Context to Actor

--- Portfoilo2/DataServiceLib/IMDbContext.cs
+++ Portfoilo2/DataServiceLib/IMDbContext.cs
@@ -15,5 +15,57 @@ namespace DataServiceLib
         public DbSet<KnownForTitles> KnownForTitles { get; set; }
         public DbSet<Principals> Principals { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseNpgsql("host=localhost;db=raw7;uid=postgres;pwd=GrimmeS0");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Actor>().ToTable("names");
+            modelBuilder.Entity<Actor>().Property(x => x.Id).HasColumnName("nameID");
+            modelBuilder.Entity<Actor>().Property(x => x.Name).HasColumnName("primaryname");
+            modelBuilder.Entity<Actor>().Property(x => x.BirthYear).HasColumnName("birthYear");
+            modelBuilder.Entity<Actor>().Property(x => x.DeathYear).HasColumnName("deathYear");
+            modelBuilder.Entity<Actor>().Property(x => x.Rating).HasColumnName("actorrating");
+
+            /*
+                        modelBuilder.Entity<Product>().ToTable("products");
+                        modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
+                        modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
+                        modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
+                        modelBuilder.Entity<Product>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+                        modelBuilder.Entity<Product>().Property(x => x.QuantityPerUnit).HasColumnName("quantityperunit");
+                        modelBuilder.Entity<Product>().Property(x => x.UnitsInStock).HasColumnName("unitsinstock");
+
+
+                                    modelBuilder.Entity<OrderDetails>().ToTable("orderdetails").HasKey(pk => new {pk.ProductId, pk.OrderId});
+                                    modelBuilder.Entity<OrderDetails>().Property(x => x.ProductId).HasColumnName("productid");
+                                    modelBuilder.Entity<OrderDetails>().Property(x => x.OrderId).HasColumnName("orderid");
+                                    modelBuilder.Entity<OrderDetails>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+                                    modelBuilder.Entity<OrderDetails>().Property(x => x.Quantity).HasColumnName("quantity");
+                                    modelBuilder.Entity<OrderDetails>().Property(x => x.Discount).HasColumnName("discount");
+                                    modelBuilder.Entity<Order>().ToTable("orders");
+                                    modelBuilder.Entity<Order>().Property(x => x.Id).HasColumnName("orderid");
+                                    modelBuilder.Entity<Order>().Property(x => x.Date).HasColumnName("orderdate");
+                                    modelBuilder.Entity<Order>().Property(x => x.Required).HasColumnName("requireddate");
+                                    modelBuilder.Entity<Order>().Property(x => x.Shipped).HasColumnName("shippeddate");
+                                    modelBuilder.Entity<Order>().Property(x => x.Freight).HasColumnName("freight");
+                                    modelBuilder.Entity<Order>().Property(x => x.ShipName).HasColumnName("shipname"); 
+                                    modelBuilder.Entity<Order>().Property(x => x.ShipCity).HasColumnName("shipcity");
+                        */
+        }
+    }
+}
+
+
+
     }
 }
