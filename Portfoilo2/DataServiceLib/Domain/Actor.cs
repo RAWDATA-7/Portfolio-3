301567a9ace601a8573ae2971afba598f72a2e9a
sfstoevring <71639425+sfstoevring@users.noreply.github.com>
2021-11-04 11:33:05 +0100

    kommentar fra en mac...

--- Portfoilo2/DataServiceLib/Domain/Actor.cs
+++ Portfoilo2/DataServiceLib/Domain/Actor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//kommentar fra en mac...
 namespace DataServiceLib.Domain
 {
     class Actor
