30e6c59c81f2aad7b2523294f63fd0ad5fc8870d
Simon <71639425+sfstoevring@users.noreply.github.com>
2021-11-09 14:16:14 +0100

    Url paging added

--- Portfoilo2/WebService/Controllers/ActorController.cs
+++ Portfoilo2/WebService/Controllers/ActorController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Routing;
 using WebService.ViewModels;
 using DataServiceLib.Domain;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System;
 
 namespace WebService.Controllers
 {
@@ -61,5 +63,48 @@ namespace WebService.Controllers
             return _linkGenerator.GetUriByName(HttpContext, nameof(GetActor), new { actor.Id });
         }
 
+
+        /*
+         * Helper stuff
+         */
+
+
+        private object ActorResultModel(UrlParam urlParam, int total, IEnumerable<ActorViewModel> model)
+        {
+            return new
+            {
+                total,
+                prev = CreatePrevPage(urlParam),
+                current = CreateCurrentPage(urlParam),
+                next = CreateNextPage(urlParam, total),
+                items = model
+            };
+        }
+
+        private string CreatePrevPage(UrlParam urlParam)
+        {
+            return urlParam.Page <= 0 ? null : GetActorsUrl(urlParam.Page - 1, urlParam.PageSize, urlParam.OrderBy);
+        }
+
+        private string CreateCurrentPage(UrlParam urlParam)
+        {
+            return GetActorsUrl(urlParam.Page, urlParam.PageSize, urlParam.OrderBy);
+        }
+
+        private string CreateNextPage(UrlParam urlParam, int total)
+        {
+            var lastPage = GetLastPage(urlParam.PageSize, total);
+            return urlParam.Page >= lastPage ? null : GetActorsUrl(urlParam.Page+1, urlParam.PageSize, urlParam.OrderBy);
+        }
+
+        private int GetLastPage(int pageSize, int total)
+        {
+            return (int)Math.Ceiling(total / (double)pageSize) - 1;
+        }
+
+        private string GetActorsUrl(int page, int pageSize, string orderBy)
+        {
+            return _linkGenerator.GetUriByName(HttpContext, nameof(GetActors), new { page, pageSize, orderBy });
+        }
     }
 }
