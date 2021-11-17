a3f995474891643c469251971ecf3ece4b9c3135
Jblomp <71639284+Jblomp@users.noreply.github.com>
2021-11-17 18:13:31 +0100

    Did some xUnit-testings

---
+++ Portfoilo2/XUnitTest/DataLayerTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using DataServiceLib;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class DataLayerTest
+    {
+
+        [Fact]
+        public void GetActorFromDatabase()
+        {
+            var service = new DataService();
+            var actor = service.GetActor("nm0185819");
+
+            Assert.Equal("Daniel Craig", actor.Name);
+        }
+
+        [Fact]
+        public void GetActorProfessionFromDatabase_LinqExpressionCall()
+        {
+            var service = new DataService();
+            var actor = service.GetActor("nm0185819");
+
+            Assert.Equal("Daniel Craig", actor.Name);
+            Assert.Equal(3, actor.Professions.Count);
+            Assert.Equal("actor", actor.Professions.ToArray().First().Name);
+        }
+
+        [Fact]
+        public void CheckPasswordService()
+        {
+            var pwservice = new PasswordService();
+            byte[] pwdHash;
+            byte[] pwdSalt;
+            pwservice.CreatePwdHash("Password", out pwdHash, out pwdSalt);
+
+            Assert.NotNull(pwdHash);
+            Assert.NotNull(pwdSalt);
+            Assert.True(pwservice.VerifyPwdHash("Password", pwdHash, pwdSalt));
+            Assert.False(pwservice.VerifyPwdHash("Password123", pwdHash, pwdSalt));
+
+        }
+    }
+}
---
+++ Portfoilo2/XUnitTest/WebServiceLayerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using DataServiceLib;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebService.Controllers;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class WebServiceLayerTest
+    {
+        private const string TitleApi = "https://localhost:5001/api/BestRatedTitles";
+
+        [Fact]
+        public void CheckDataInBestRatedTitleList()
+        {
+            var (data, statusCode) = GetObject(TitleApi);
+
+           var list = data.Properties().Select(x => x.Value).ToList();
+
+
+            Assert.Equal("Breaking Bad", list[4][0]["name"]);
+            Assert.Equal("tt0903747", list[4][0]["id"]);
+        }
+
+        /*
+         * 
+         * Helper Stuff
+         * 
+         */
+        (JObject, HttpStatusCode) GetObject(string url)
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+            var data = response.Content.ReadAsStringAsync().Result;
+            return ((JObject)JsonConvert.DeserializeObject(data), response.StatusCode);
+        }
+    }
+}
