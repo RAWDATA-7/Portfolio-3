447c01e30b57e6d626edb5a59acabc0f3b91515b
aheide <71612663+aheide@users.noreply.github.com>
2021-12-16 23:09:31 +0100

    fixed col values + made images centered in their columns

--- Portfolio3/DataServiceLib/IMDbContext.cs
+++ Portfolio3/DataServiceLib/IMDbContext.cs
@@ -34,8 +34,9 @@ namespace DataServiceLib
         {
             base.OnConfiguring(optionsBuilder);
         //add path file here or hardcode in line 43
-        string path = System.IO.File.ReadAllText(@"C:\Users\JesperBlom\hello\Portfolio-3\Portfolio3\path.txt");
+        //string path = System.IO.File.ReadAllText(@"C:\Users\JesperBlom\hello\Portfolio-3\Portfolio3\path.txt");
         //string path = System.IO.File.ReadAllText(@"C:\Users\sfsto\Documents\GitHub\Portfolio2\Portfoilo2\path.txt");
+          string path = System.IO.File.ReadAllText(@"C:\Users\ander\Documents\DOX\RUC\RAWDATA\Portfolio3\Portfolio-3\Portfolio3\path.txt");
 
             optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
             optionsBuilder.EnableSensitiveDataLogging();
